cc51fa13fd3eeb2a49ec3edceaf7a3e4fb7e3989
HepoHB <NeroHaziel@outlook.com>
2023-06-03 01:23:01 -0300

    Initial Commit - ListEmployees

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
-print('Hello World!')
-a = int(input('Var A:'))
-b = int(input('Var B:'))
-print(a+b)
+import pandas as pd
+
+class JSonRequests:
+    def __init__(self):
+        self.employeesDataFrame = pd.read_json('data/data_AppleEmployee.json')
+        pass
+    
+    def ListEmployees(self, status=None):
+        '''Possible Parameters: None | Active | Vacation | Retired'''
+        if status is not None:
+            for index, row in self.employeesDataFrame.iterrows():
+                if (row[0]['status'] == status):
+                    print(row[0]['name'])
+                elif (row[0]['status'] == status):
+                    print(row[0]['name'])
+                elif (row[0]['status'] == status):
+                    print(row[0]['name'])
+                else:
+                    pass
+        else:
+            for index, row in self.employeesDataFrame.iterrows():
+                print(row[0]['name'])
+            
+
+PDJson = JSonRequests()
+PDJson.ListEmployees()
